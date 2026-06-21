69a43eabfb3deda68a56ddfb2e3c6015428baf8e
maneloco <manelmr2015@gmail.com>
2026-06-21 12:28:53 +0100

    He refactorizado el código, separando la lógica de la operación xor

--- src/encrypt.py
+++ src/encrypt.py
@@ -9,37 +9,30 @@ def to_number(message: str) -> bytearray:
 def from_number(numbers: bytearray) -> str:
     _, reverse = get_character_table()
     nums: List[int] = list(numbers)
-    result: str= ""
-    for num in nums:
-        result += reverse[num]
+    result: str = "".join([reverse[num] for num in numbers])
     return result
 
 
 def xor_encryption(message: str, key: str) -> Tuple[str, List[bytearray]]:
     message_bytes = to_number(message)
     key_bytes = to_number(key)
-    encrypted_message = bytearray()
-
-    i = 0
-    j = 0
-    while i < len(message_bytes):
-        xor = message_bytes[i] ^ key_bytes[j]
-        encrypted_message.append(xor)
-        i += 1
-        j = (j + 1) % len(key_bytes)
+    encrypted_message = xor_operation(message_bytes, key_bytes)
     
     return encrypted_message, key_bytes
 
 def xor_decryption(message_encrypted: bytearray, key: bytearray) -> Tuple(str, str):
-    decrypted_message = bytearray()
-    _, reverse = get_character_table()
+    decrypted_message = xor_operation(message_encrypted, key)
 
-    i = 0
-    j = 0
-    while i < len(message_encrypted):
-        xor = message_encrypted[i] ^ key[j]
-        decrypted_message.append(xor)
-        i += 1
-        j = (j + 1) % len(key)
     return from_number(decrypted_message), from_number(key)
+
+
+def xor_operation(bytes_a: bytearray, bytes_b: bytearray) -> bytearray:
+    result = bytearray()
+    i, j = 0, 0
+    while i < len(bytes_a):
+        xor = bytes_a[i] ^ bytes_b[j]
+        result.append(xor)
+        i += 1
+        j = (j + 1) % len(bytes_b)
+    return result
 
